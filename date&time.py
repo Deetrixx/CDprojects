12e90ab8b08668ad9fe3a4cafa7b79bfe2c6e169
Deetrixx <dennisgithua30@gmail.com>
2016-05-17 14:05:38 +0300

    extracting information

--- date&time.py
+++ date&time.py
@@ -2,3 +2,20 @@
 from datetime import datetime
 now=datetime.now()
 print now
+
+#extracting information
+from datetime import datetime
+now=datetime.now()
+current_year=now.year
+current_month=now.month
+current_day=now.day
+
+print now.year
+print now.month
+print now.day
+
+#Hot Date
+from datetime import datetime
+now = datetime.now()
+
+print '%s/%s/%s' % (now.month, now.day, now.year)
